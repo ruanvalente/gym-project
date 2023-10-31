11c69b219f7f6a25626380a8eec90a72f25b8832
ruanvalente <ruanopensuselinux@gmail.com>
2023-10-31 14:47:47 -0300

    WIP: mock data table

--- frontend/src/pages/student-list/index.tsx
+++ frontend/src/pages/student-list/index.tsx
@@ -19,18 +19,16 @@ export function StudentList() {
           <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-white dark:text-gray-400">
               <tr>
-                <th scope="col" className="px-6 py-3">
-                  Product name
+                <th scope="col" className="px-6 py-3 uppercase">
+                  Nome
                 </th>
-                <th scope="col" className="px-6 py-3">
-                  Color
+                <th scope="col" className="px-6 py-3 uppercase">
+                  E-mail
                 </th>
-                <th scope="col" className="px-6 py-3">
-                  Category
+                <th scope="col" className="px-6 py-3 uppercase">
+                  Idade
                 </th>
-                <th scope="col" className="px-6 py-3">
-                  Price
-                </th>
+                <th scope="col" className="px-6 py-3"></th>
               </tr>
             </thead>
             <tbody>
@@ -39,33 +37,42 @@ export function StudentList() {
                   scope="row"
                   className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                 >
-                  Apple MacBook Pro 17"
+                  Cha Ji-Hun
                 </th>
-                <td className="px-6 py-4">Silver</td>
-                <td className="px-6 py-4">Laptop</td>
-                <td className="px-6 py-4">$2999</td>
+                <td className="px-6 py-4">example@email.com</td>
+                <td className="px-6 py-4">22</td>
+                <td className="px-6 py-4">
+                  <button className="mr-4 text-blue-400">editar</button>
+                  <button className="ml-4 text-red-400">apagar</button>
+                </td>
               </tr>
               <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                 <th
                   scope="row"
                   className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                 >
-                  Microsoft Surface Pro
+                  Darius Cummings
                 </th>
-                <td className="px-6 py-4">White</td>
-                <td className="px-6 py-4">Laptop PC</td>
-                <td className="px-6 py-4">$1999</td>
+                <td className="px-6 py-4">example@email.com</td>
+                <td className="px-6 py-4">33</td>
+                <td className="px-6 py-4">
+                  <button className="mr-4 text-blue-400">editar</button>
+                  <button className="ml-4 text-red-400">apagar</button>
+                </td>
               </tr>
               <tr className="bg-white dark:bg-gray-800">
                 <th
                   scope="row"
                   className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                 >
-                  Magic Mouse 2
+                  Carmelita Marsham
                 </th>
-                <td className="px-6 py-4">Black</td>
-                <td className="px-6 py-4">Accessories</td>
-                <td className="px-6 py-4">$99</td>
+                <td className="px-6 py-4">example@email.com</td>
+                <td className="px-6 py-4">24</td>
+                <td className="px-6 py-4">
+                  <button className="mr-4 text-blue-400">editar</button>
+                  <button className="ml-4 text-red-400">apagar</button>
+                </td>
               </tr>
             </tbody>
           </table>
